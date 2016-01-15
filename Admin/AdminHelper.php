0cf7d630df95a5483be21e8f9d71f81907384231
Daniele Artico <daniele.artico@cogitoweb.it>
2016-01-15 17:52:56 +0100

    Handle multi-level nested Collections

--- Admin/AdminHelper.php
+++ Admin/AdminHelper.php
@@ -11,6 +11,7 @@
 
 namespace Sonata\AdminBundle\Admin;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Inflector\Inflector;
 use Doctrine\Common\Util\ClassUtils;
 use Sonata\AdminBundle\Exception\NoValueException;
@@ -18,6 +19,7 @@ use Sonata\AdminBundle\Util\FormBuilderIterator;
 use Sonata\AdminBundle\Util\FormViewIterator;
 use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 
 class AdminHelper
 {
@@ -79,107 +81,170 @@ class AdminHelper
         return $this->pool->getInstance($code);
     }
 
-    /**
-     * Note:
-     *   This code is ugly, but there is no better way of doing it.
-     *   For now the append form element action used to add a new row works
-     *   only for direct FieldDescription (not nested one).
-     *
-     * @throws \RuntimeException
-     *
-     * @param \Sonata\AdminBundle\Admin\AdminInterface $admin
-     * @param object                                   $subject
-     * @param string                                   $elementId
-     *
-     * @return array
-     */
-    public function appendFormFieldElement(AdminInterface $admin, $subject, $elementId)
-    {
-        // retrieve the subject
-        $formBuilder = $admin->getFormBuilder();
+//    /**
+//     * Note:
+//     *   This code is ugly, but there is no better way of doing it.
+//     *   For now the append form element action used to add a new row works
+//     *   only for direct FieldDescription (not nested one).
+//     *
+//     * @throws \RuntimeException
+//     *
+//     * @param \Sonata\AdminBundle\Admin\AdminInterface $admin
+//     * @param object                                   $subject
+//     * @param string                                   $elementId
+//     *
+//     * @return array
+//     */
+//    public function appendFormFieldElement(AdminInterface $admin, $subject, $elementId)
+//    {
+//        // retrieve the subject
+//        $formBuilder = $admin->getFormBuilder();
+//
+//        $form = $formBuilder->getForm();
+//        $form->setData($subject);
+//        $form->submit($admin->getRequest());
+//
+//        // get the field element
+//        $childFormBuilder = $this->getChildFormBuilder($formBuilder, $elementId);
+//
+//        // retrieve the FieldDescription
+//        $fieldDescription = $admin->getFormFieldDescription($childFormBuilder->getName());
+//
+//        try {
+//            $value = $fieldDescription->getValue($form->getData());
+//        } catch (NoValueException $e) {
+//            $value = null;
+//        }
+//
+//        // retrieve the posted data
+//        $data = $admin->getRequest()->get($formBuilder->getName());
+//
+//        if (!isset($data[$childFormBuilder->getName()])) {
+//            $data[$childFormBuilder->getName()] = array();
+//        }
+//
+//        $objectCount = count($value);
+//        $postCount   = count($data[$childFormBuilder->getName()]);
+//
+//        $fields = array_keys($fieldDescription->getAssociationAdmin()->getFormFieldDescriptions());
+//
+//        // for now, not sure how to do that
+//        $value = array();
+//        foreach ($fields as $name) {
+//            $value[$name] = '';
+//        }
+//
+//        // add new elements to the subject
+//        while ($objectCount < $postCount) {
+//            // append a new instance into the object
+//            $this->addNewInstance($form->getData(), $fieldDescription);
+//            ++$objectCount;
+//        }
+//
+//        $this->addNewInstance($form->getData(), $fieldDescription);
+//        $data[$childFormBuilder->getName()][] = $value;
+//
+//        $finalForm = $admin->getFormBuilder()->getForm();
+//        $finalForm->setData($subject);
+//
+//        // bind the data
+//        $finalForm->setData($form->getData());
+//
+//        return array($fieldDescription, $finalForm);
+//    }
 
-        $form = $formBuilder->getForm();
-        $form->setData($subject);
-        $form->submit($admin->getRequest());
+	/**
+	 * 2016-01-15 Daniele Artico
+	 * Method is completely rewritten to improve code readability
+	 *
+	 * @throws \RuntimeException
+	 *
+	 * @param \Sonata\AdminBundle\Admin\AdminInterface $admin
+	 * @param object                                   $subject
+	 * @param string                                   $elementId
+	 *
+	 * @return \Symfony\Component\Form\Form
+	 */
+	public function appendFormFieldElement(AdminInterface $admin, $subject, $elementId)
+	{
+		// retrieve the subject
+		$form = $admin->getFormBuilder()->getForm();
+		$form->setData($subject);
+		$form->submit($admin->getRequest());
 
-        // get the field element
-        $childFormBuilder = $this->getChildFormBuilder($formBuilder, $elementId);
+		$elementId = preg_replace('#.(\d+)#', '[$1]', implode('.', explode('_', substr($elementId, strpos($elementId, '_') + 1))));
 
-        // retrieve the FieldDescription
-        $fieldDescription = $admin->getFormFieldDescription($childFormBuilder->getName());
+		// append a new instance into the object
+		$this->addNewInstance($admin, $elementId);
 
-        try {
-            $value = $fieldDescription->getValue($form->getData());
-        } catch (NoValueException $e) {
-            $value = null;
-        }
+		// return new form with empty row
+		$finalForm = $admin->getFormBuilder()->getForm();
+		$finalForm->setData($subject);
 
-        // retrieve the posted data
-        $data = $admin->getRequest()->get($formBuilder->getName());
+		// bind the data
+		$finalForm->setData($form->getData());
 
-        if (!isset($data[$childFormBuilder->getName()])) {
-            $data[$childFormBuilder->getName()] = array();
-        }
+		return $finalForm;
+	}
 
-        $objectCount = count($value);
-        $postCount   = count($data[$childFormBuilder->getName()]);
+//    /**
+//     * Add a new instance to the related FieldDescriptionInterface value.
+//     *
+//     * @param object                                              $object
+//     * @param \Sonata\AdminBundle\Admin\FieldDescriptionInterface $fieldDescription
+//     *
+//     * @throws \RuntimeException
+//     */
+//    public function addNewInstance($object, FieldDescriptionInterface $fieldDescription)
+//    {
+//        $instance = $fieldDescription->getAssociationAdmin()->getNewInstance();
+//        $mapping  = $fieldDescription->getAssociationMapping();
+//
+//        $method = sprintf('add%s', $this->camelize($mapping['fieldName']));
+//
+//        if (!method_exists($object, $method)) {
+//            $method = rtrim($method, 's');
+//
+//            if (!method_exists($object, $method)) {
+//                $method = sprintf('add%s', $this->camelize(Inflector::singularize($mapping['fieldName'])));
+//
+//                if (!method_exists($object, $method)) {
+//                    throw new \RuntimeException(sprintf('Please add a method %s in the %s class!', $method, ClassUtils::getClass($object)));
+//                }
+//            }
+//        }
+//
+//        $object->$method($instance);
+//    }
 
-        $fields = array_keys($fieldDescription->getAssociationAdmin()->getFormFieldDescriptions());
+	/**
+	 * 2016-01-15 Daniele Artico
+	 * Method is completely rewritten to improve code readability
+	 *
+	 * @param AdminInterface $admin
+	 * @param string         $elementId
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	public function addNewInstance(AdminInterface $admin, FieldDescriptionInterface $elementId)
+	{
+		$entity = $admin->getSubject();
 
-        // for now, not sure how to do that
-        $value = array();
-        foreach ($fields as $name) {
-            $value[$name] = '';
-        }
+		$propertyAccessor = new PropertyAccessor();
+		$collection       = $propertyAccessor->getValue($entity, $elementId);
 
-        // add new elements to the subject
-        while ($objectCount < $postCount) {
-            // append a new instance into the object
-            $this->addNewInstance($form->getData(), $fieldDescription);
-            ++$objectCount;
-        }
+		if ($collection instanceof ArrayCollection) {
+			$entityClassName = $this->entityClassNameFinder($admin, explode('.', preg_replace('#\[\d*?\]#', '', $elementId)));
+		} elseif ($collection instanceof \Doctrine\ORM\PersistentCollection) {
+			// Since Doctrine 2.4
+			$entityClassName = $collection->getTypeClass()->getName();
+		} else {
+			throw new \InvalidArgumentException(sprintf('%s is not a valid Collection class', get_class($collection)));
+		}
 
-        $this->addNewInstance($form->getData(), $fieldDescription);
-        $data[$childFormBuilder->getName()][] = $value;
-
-        $finalForm = $admin->getFormBuilder()->getForm();
-        $finalForm->setData($subject);
-
-        // bind the data
-        $finalForm->setData($form->getData());
-
-        return array($fieldDescription, $finalForm);
-    }
-
-    /**
-     * Add a new instance to the related FieldDescriptionInterface value.
-     *
-     * @param object                                              $object
-     * @param \Sonata\AdminBundle\Admin\FieldDescriptionInterface $fieldDescription
-     *
-     * @throws \RuntimeException
-     */
-    public function addNewInstance($object, FieldDescriptionInterface $fieldDescription)
-    {
-        $instance = $fieldDescription->getAssociationAdmin()->getNewInstance();
-        $mapping  = $fieldDescription->getAssociationMapping();
-
-        $method = sprintf('add%s', $this->camelize($mapping['fieldName']));
-
-        if (!method_exists($object, $method)) {
-            $method = rtrim($method, 's');
-
-            if (!method_exists($object, $method)) {
-                $method = sprintf('add%s', $this->camelize(Inflector::singularize($mapping['fieldName'])));
-
-                if (!method_exists($object, $method)) {
-                    throw new \RuntimeException(sprintf('Please add a method %s in the %s class!', $method, ClassUtils::getClass($object)));
-                }
-            }
-        }
-
-        $object->$method($instance);
-    }
+		$collection->add(new $entityClassName);
+		$propertyAccessor->setValue($entity, $elementId, $collection);
+	}
 
     /**
      * Camelize a string.
@@ -194,4 +259,22 @@ class AdminHelper
     {
         return BaseFieldDescription::camelize($property);
     }
+
+	/**
+	 * 2016-01-15 Daniele Artico
+	 *
+	 * @param AdminInterface $admin
+	 * @param array          $elements
+	 */
+	protected function entityClassNameFinder(AdminInterface $admin, $elements)
+	{
+		$element = array_shift($elements);
+		$associationAdmin = $admin->getFormFieldDescription($element)->getAssociationAdmin();
+
+		if (count($elements) == 0) {
+			return $associationAdmin->getClass();
+		} else {
+			return $this->entityClassNameFinder($associationAdmin, $elements);
+		}
+	}
 }
